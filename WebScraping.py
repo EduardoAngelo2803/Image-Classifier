d013d80259441e99d37257ba3ce17cba3df9cb84
Eduardoangelo2803 <leam@cesar.school>
2024-06-21 15:37:10 -0300

    Change Dir Names

--- WebScraping.py
+++ WebScraping.py
@@ -1,13 +1,5 @@
-import torch
+from bing_image_downloader import downloader
 
-print("Versão do PyTorch:", torch.__version__)
-print("Versão do CUDA disponível no PyTorch:", torch.version.cuda)
-print("Quantas GPUs estão disponíveis:", torch.cuda.device_count())
-print("Nome da GPU (se disponível):", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "Nenhuma GPU encontrada")
+downloader.download("Camisas do Nautico", limit=100, output_dir='imagens/scrapping/nautico', force_replace=False, timeout=2,verbose=True)
 
-
-
-
-##from bing_image_downloader import downloader
-
-## ownloader.download("Camisas do Nautico", limit=100, output_dir='imagens/testscrapping/nautico', force_replace=False, timeout=2,verbose=True)
+downloader.download("Camisas do Sport", limit=100, output_dir='imagens/scrapping/sport', force_replace=False, timeout=2,verbose=True)
